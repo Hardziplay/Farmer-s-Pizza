8c0774cb24890a04f3093d9b8bc1a4a7991f7675
Hardziplay <128897299+Hardziplay@users.noreply.github.com>
2024-06-25 17:19:12 +0300

    added cheese item

--- src/main/java/net/hardzi/farmerspizza/FarmersPizza.java
+++ src/main/java/net/hardzi/farmerspizza/FarmersPizza.java
@@ -2,7 +2,6 @@ package net.hardzi.farmerspizza;
 
 import com.mojang.logging.LogUtils;
 import net.hardzi.farmerspizza.item.ModItems;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
