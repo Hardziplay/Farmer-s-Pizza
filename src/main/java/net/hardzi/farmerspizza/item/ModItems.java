c5a923403ea96e5fdceb244b9b4b1464f02a906b
Hardziplay <128897299+Hardziplay@users.noreply.github.com>
2024-06-25 17:23:50 +0300

    added grated cheese item

--- src/main/java/net/hardzi/farmerspizza/item/ModItems.java
+++ src/main/java/net/hardzi/farmerspizza/item/ModItems.java
@@ -12,6 +12,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, FarmersPizza.MOD_ID);
 
     public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GRATED_CHEESE = ITEMS.register("grated_cheese", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
